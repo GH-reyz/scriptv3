8f9d795713ed63b221ab99e168789b39f033affa
GH-reyz <106010948+GH-reyz@users.noreply.github.com>
2022-07-29 17:35:57 +0800

    Add files via upload

--- menu.sh
+++ menu.sh
@@ -39,47 +39,7 @@ username=$(cat /usr/bin/user)
 oid=$(cat /usr/bin/ver)
 exp=$(cat /usr/bin/e)
 clear
-bulan=$(date +%b)
-vnstat_profile=$(vnstat | sed -n '3p' | awk '{print $1}' | grep -o '[^:]*')
-vnstat -i ${vnstat_profile} >/root/t1
-today=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $8}')
-today_v=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $9}')
-today_rx=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $2}')
-today_rxv=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $3}')
-today_tx=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $5}')
-today_txv=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $6}')
-if [ "$(grep -wc ${bulan} /root/t1)" != '0' ]; then
-  bulan=$(date +%b)
-  month=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $9}')
-  month_v=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $10}')
-  month_rx=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $3}')
-  month_rxv=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $4}')
-  month_tx=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $6}')
-  month_txv=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $7}')
-else
-  bulan=$(date +%Y-%m)
-  month=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $8}')
-  month_v=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $9}')
-  month_rx=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $2}')
-  month_rxv=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $3}')
-  month_tx=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $5}')
-  month_txv=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $6}')
-fi
-if [ "$(grep -wc yesterday /root/t1)" != '0' ]; then
-  yesterday=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $8}')
-  yesterday_v=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $9}')
-  yesterday_rx=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $2}')
-  yesterday_rxv=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $3}')
-  yesterday_tx=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $5}')
-  yesterday_txv=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $6}')
-else
-  yesterday=NULL
-  yesterday_v=NULL
-  yesterday_rx=NULL
-  yesterday_rxv=NULL
-  yesterday_tx=NULL
-  yesterday_txv=NULL
-fi
+
 # STATUS EXPIRED ACTIVE
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[4$below" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}(Active)${Font_color_suffix}"
@@ -121,9 +81,9 @@ clear
 echo -e "\e[$banner_colour"
 figlet -f $ascii "$banner"
 echo -e "\e[$text Premium Script "
-echo -e " \e[$line════════════════════════════════════════════════════════════\e[m"
-echo -e " \e[$back_text                    \e[30m[\e[$box SERVER INFORMATION\e[30m ]\e[1m                  \e[m"
-echo -e " \e[$line════════════════════════════════════════════════════════════\e[m"
+echo -e   " \e[$line════════════════════════════════════════════════════════════\e[m"
+echo -e   " \e[$back_text                    \e[30m[\e[$box SERVER INFORMATION\e[30m ]\e[1m                  \e[m"
+echo -e   " \e[$line════════════════════════════════════════════════════════════\e[m"
 echo -e "  \e[$text Cpu Model            :$cname"
 echo -e "  \e[$text Number Of Core       : $cores"
 echo -e "  \e[$text Cpu Frequency        :$freq MHz"
@@ -140,29 +100,24 @@ echo -e "  \e[$text Order ID             : $oid"
 echo -e "  \e[$text Expired Status       : $exp $sts"
 echo -e "  \e[$text Certificate Status   : Expired in $certifacate days"
 echo -e "  \e[$text Provided By          : $creditt"
-echo -e " \e[$line════════════════════════════════════════════════════════════\e[m"
-echo -e " TRAFFIC           TODAY          YESTERDAY          MONTH" | lolcat
-echo -e " UPLOAD            $today_tx $today_txv        $yesterday_tx $yesterday_txv          $month_tx $month_txv"
-echo -e " DOWNLOAD          $today_rx $today_rxv        $yesterday_rx $yesterday_rxv         $month_rx $month_rxv"
-echo -e " TOTAL             $today $today_v                $yesterday $yesterday_v                $month $month_v"
-echo -e " \e[$line════════════════════════════════════════════════════════════\e[m"
-echo -e " \e[$back_text                        \e[30m[\e[$box MAIN MENU\e[30m ]\e[1m                       \e[m"
-echo -e " \e[$line════════════════════════════════════════════════════════════\e[m"
-echo -e "  \e[$number [•1]\e[m \e[$below XRAY VMESS & VLESS\e[m          \e[$number [•7]\e[m \e[$below MENU THEMES\e[m"
-echo -e "  \e[$number [•2]\e[m \e[$below TROJAN XRAY & GO\e[m            \e[$number [•8]\e[m \e[$below CLEAR LOG VPS\e[m"
-echo -e "  \e[$number [•3]\e[m \e[$below PANEL WIREGUARDS\e[m            \e[$number [•9]\e[m \e[$below CHANGE PORT\e[m"
-echo -e "  \e[$number [•4]\e[m \e[$below PANEL SHADOWSOCKS\e[m           \e[$number [10]\e[m \e[$below CHECK RUNNING\e[m"
-echo -e "  \e[$number [•5]\e[m \e[$below OPENSSH & OPENVPN\e[m           \e[$number [11]\e[m \e[$below REBOOT VPS\e[m"
-echo -e "  \e[$number [•6]\e[m \e[$below SYSTEM MENU\e[m                 \e[$number [12]\e[m \e[$below INFO ALL PORT\e[m"
-echo -e " \e[$line════════════════════════════════════════════════════════════\e[m"
-echo -e "  \e[$below 🔰 Premium VPS by $creditt 🔰"
-echo -e "  \e[$below 🔰 Thank you for using script by ReyzV4 🔰"
-echo -e " \e[$line════════════════════════════════════════════════════════════\e[m"
-echo -e ""
-echo -e "  \e[$below [Ctrl + C] For exit from main menu\e[m"
-echo -e "\e[$below "
-read -p "   Select From Options [1-12 or x] :  " menu
-echo -e ""
+echo -e   " \e[$line════════════════════════════════════════════════════════════\e[m"
+echo -e   " \e[$back_text                        \e[30m[\e[$box MAIN MENU\e[30m ]\e[1m                       \e[m"
+echo -e   " \e[$line════════════════════════════════════════════════════════════\e[m"
+echo -e   "  \e[$number [•1]\e[m \e[$below XRAY VMESS & VLESS\e[m          \e[$number [•7]\e[m \e[$below MENU THEMES\e[m"
+echo -e   "  \e[$number [•2]\e[m \e[$below TROJAN XRAY & GO\e[m            \e[$number [•8]\e[m \e[$below CLEAR LOG VPS\e[m"
+echo -e   "  \e[$number [•3]\e[m \e[$below PANEL WIREGUARDS\e[m            \e[$number [•9]\e[m \e[$below CHANGE PORT\e[m"
+echo -e   "  \e[$number [•4]\e[m \e[$below PANEL SHADOWSOCKS\e[m           \e[$number [10]\e[m \e[$below CHECK RUNNING\e[m"
+echo -e   "  \e[$number [•5]\e[m \e[$below OPENSSH & OPENVPN\e[m           \e[$number [11]\e[m \e[$below REBOOT VPS\e[m"
+echo -e   "  \e[$number [•6]\e[m \e[$below SYSTEM MENU\e[m                 \e[$number [12]\e[m \e[$below INFO ALL PORT\e[m"
+echo -e   " \e[$line════════════════════════════════════════════════════════════\e[m"
+echo -e   "  \e[$below 🔰 Premium VPS by $creditt 🔰"
+echo -e   "  \e[$below 🔰 Thank you for using script by ReyzV4 🔰"
+echo -e   " \e[$line════════════════════════════════════════════════════════════\e[m"
+echo -e   ""
+echo -e   "  \e[$below [Ctrl + C] For exit from main menu\e[m"
+echo -e   "\e[$below "
+read -p   "   Select From Options [1-12 or x] :  " menu
+echo -e   ""
 case $menu in
 1)
 xraay
